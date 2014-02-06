2aa11325aedf11ad1c05cf079ff60ae6d4706c4b
Maxime Oquab <moquab@tesla.rocq.inria.fr>
2014-02-06 16:51:59 +0100

    gradWeight lua prototype

--- extra/nxn/Prototype-Of-Convolution.lua
+++ extra/nxn/Prototype-Of-Convolution.lua
@@ -518,12 +518,237 @@ end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function ComputeGradWeights(gradOutput, input, kernel, parms)
-   gradweight = kernel:clone():zero()
+   -- typecheck
+   local type = torch.typename(input)
+   assert(type == "torch.FloatTensor" or type == "torch.DoubleTensor")
+   assert(torch.typename(kernel) == type)
+   assert(torch.typename(result) == type)
+   
+   -- input
+   assert(input:nDimension() == 4)
+   local bs = input:size(1) -- batch size
+   local ih = input:size(2) -- input height
+   local iw = input:size(3) -- input width
+   local ip = input:size(4) -- input planes
+      
+      -- kernel
+      assert(kernel:nDimension() == 4)
+   local kh = kernel:size(1) -- kernel height
+   local op = kernel:size(2) -- output planes
+   local kw = kernel:size(3) -- kernel width
+      assert(ip == kernel:size(4)) -- input planes
+      
+      -- optional parameters
+      parms = parms or {}
+   local padleft = parms.padleft or 0; -- input padding
+   local padtop = parms.padtop or 0;
+   local padright = parms.padright or 0;
+   local padbottom = parms.padbottom or 0;
+   local stridex = parms.stridex or 1; -- convolution stride
+   local stridey = parms.stridey or 1;
+   local reverse = parms.reverse or false; -- reversed kernel for backpropagation
+   local exact = parms.exact; -- error if padding is not exact
+   local overlap = parms.overlap; -- whether blas takes overlapped matrices
+   local alpha = parms.alpha or 1; -- result=alpha*result+beta*convolution
+   local beta = parms.beta or 0;
+   assert(padleft>=0 and padtop>=0 and padright>=0 and padbottom>=0)
+   assert(stridex>=1 and stridey>=1)
+   
+   -- compute output size
+   local ow = math.floor((iw + padleft + padright - kw) / stridex) + 1
+   local oh = math.floor((ih + padtop + padbottom - kh) / stridey) + 1
+   
+   -- correct padright and padbottom
+   local oldpadright = padright
+   local oldpadbottom = padbottom
+   padright = ow * stridex + kw - stridex - iw - padleft;
+   padbottom = oh * stridey + kh - stridey - ih - padtop;
+   assert(not exact or padright ~= oldpadright, "horizontal size mismatch");
+   assert(not exact or padbottom ~= oldpadbottom, "horizontal size mismatch");
+   if padright < 0 then padright = 0 end
+   if padbottom < 0 then padbottom = 0 end
+   
+   -- input size with padding
+   local piw = padleft + iw + padright; 
+   local pih = padtop + ih + padbottom;
+   
+   -- number of horizontal strides between nonoverlapping runs
+   local nxs = 1
+   if not overlap then
+      nxs = math.floor((kw + stridex - 1) / stridex)
+   end
+   
+   -- total size of output buffer
+   local tow = math.floor((piw + stridex - 1) / stridex)
+   local toh = math.floor((pih + stridey - 1) / stridey)
+   
+   -- total size of input and output buffers
+   local tiw = tow * stridex;
+   local tih = toh * stridey;  
+   assert(tiw >= piw and piw >= iw)
+   assert(tih >= pih and pih >= ih)
+   
+   -- copy image into input buffer
+   local icopy =  newSameTensor(input, stridey, bs, toh, tiw, ip):zero()
    
    
+   for s=0,stridey-1 do
+      local fout = math.floor((math.max(0,padtop-s)+stridey-1)/stridey)
+      local fin = fout * stridey - padtop + s
+      assert(fout >= 0 and fin >= 0)
+      local icopyptr=torch.data(icopy)
+      local inputptr=torch.data(input)
+      if fin < ih then
+         --inputstride2 = (input:stride())[2]*stridey
+         local inputsize2   = math.floor(((ih-fin) + stridey - 1) / stridey)
+         local iticopy0=s*bs*toh*tiw*ip
+         local itinput0=0
+         for it1=1,bs do
+            local iticopy1=iticopy0+(it1-1)*toh*tiw*ip
+            local itinput1=itinput0+(it1-1)*ih*iw*ip
+            
+            for it2=1, inputsize2 do
+               local iticopy2=iticopy1+(fout+it2-1)*tiw*ip
+               local itinput2=itinput1+((fin+1)+(it2-1)*stridey-1)*iw*ip
+               
+               for it3=1,input:size(3) do
+                  local iticopy3=iticopy2+(padleft+it3-1)*ip
+                  local itinput3=itinput2+(it3-1)*ip
+                  
+                  for it4=1,ip do
+                     
+                     icopyptr[iticopy3]=inputptr[itinput3]
+                     iticopy3=iticopy3+1
+                     itinput3=itinput3+1
+                  end
+               end
+            end
+         end
+      else
+         
+         for it=bs*toh*tiw*ip*s, bs*toh*tiw*ip*(s+1)-1 do
+            icopyptr[it]=0
+         end
+      end
+   end
+   --print('copy time : '..z:time().real)
+   
+   
+   
+   
+   
+   --
+   --
+   -- this is where it starts
+   --
+   --
+   
+   
+   
+   
+   
+   -- copy kernel into kernel buffer
+   --   local kcopy = copySpatialConvolutionKernel(kernel,reverse)
+   
+   -- create the gradWeights matrix
+   --local kcopy = copySpatialConvolutionKernel(kernel,reverse):zero()
+   local kcopy = newSameTensor(kernel, (#kernel)[1], (#kernel)[2], (#kernel)[3], (#kernel)[4]):zero()
+   
+   
+   
+   
+   -- allocate and clear output buffer
+   -- local ocopy = newSameTensor(result, bs, toh, tow, op)
+   -- ocopy:fill(0)
+   
+   -- copy gradOutput into ocopy
+   local ocopy = newSameTensor(gradOutput, bs, toh, tow, op)
+   ocopy:fill(0)
+   
+   local tocopy = ocopy:narrow(2,1,oh):narrow(3,1,ow)
+   tocopy:copy(gradOutput)
+   
+   
+   
+   
+   -- call GEMM
+   for hcall =0,nxs-1 do
+      for vcall = 0,kh-1 do
+         local sq = math.floor(vcall / stridey)
+         local sr = vcall - sq * stridey
+         --   local icopy =  newSameTensor(input, stridey, bs, toh, tiw, ip)
+         local iptr = torch.data(icopy[{sr+1,{},sq+1,hcall*stridex+1,{}}])
+         local kptr = torch.data(kcopy:select(1,vcall+1))
+         local optr = torch.data(ocopy:select(3,hcall+1))
+         local nrun = (bs-1)*toh*tow + oh*tow
+         local ngem = math.floor((nrun - hcall) / nxs)
+         --         GEMM(type,'T','N', op, ngem, kw*ip, 
+         --              1, kptr, kw*ip, iptr, nxs*stridex*ip,
+         --              1, optr, nxs*op ) 
+         
+         -- void THFloatBlas_gemm(char transa, char transb, long m, long n, long k, 
+         --                       float alpha, float *a, long lda, float *b, long ldb, 
+         --                      float beta, float *c, long ldc);         
+         
+         
+         GEMM(type,'N','T',  kw*ip,op, ngem, 
+              1, iptr, nxs*stridex*ip, optr, nxs*op, 
+              1, kptr, kw*ip) 
+      end
+   end
+   
+   if false then
+      -- accumulate output chunk into result tensor
+      result:resize(bs,oh,ow,op)
+      local tocopy = ocopy:narrow(2,1,oh):narrow(3,1,ow)
+      if beta == 0 and alpha == 1 then
+         result:copy(tocopy)
+      elseif beta == 1 then
+         result.add(tocopy, alpha)
+      else
+         result.mul(beta)
+         result.add(tocopy, value)
+      end
+      --return result   
+   end
+   
+   return kcopy
    
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
