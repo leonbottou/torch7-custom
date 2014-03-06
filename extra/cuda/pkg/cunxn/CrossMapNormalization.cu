ae994150dfe41a6a5479b84c68b6c956496190bb
Maxime Oquab <moquab@tesla.rocq.inria.fr>
2014-03-06 19:22:24 +0100

    fixed cmrnorm backprop on GPU (.....)

--- extra/cuda/pkg/cunxn/CrossMapNormalization.cu
+++ extra/cuda/pkg/cunxn/CrossMapNormalization.cu
@@ -7,113 +7,109 @@
 #endif
 
 /*
- * Description:
- *    this function crossmap-normalizes one single row
- *    along the innermost dimension
- *    Nd input, Nd output
+ * This could be improved by using registers + __shfl
  */
 
 
-template <int maxnumplanes> __global__ void CrossMapNormalization_output(float *input, float *output, float *z, const int nPlanes, const float k, const float alpha, const float beta, const int n)
+
+template <int maxnumplanes> __global__ void CrossMapNormalization_output(float *input, float *output, float *zsave, int iw, int nplanes, const float k, const float alpha, const float beta, const int n, int str0, int str1)
 {
-  // one block = one pixel (x size feature maps)
-  const int pixidx = blockIdx.y * gridDim.x + blockIdx.x;
-  const int blk=blockDim.x;
-  const int tidx = threadIdx.x;
-  const int valuesperthread = nPlanes/blockDim.x;
-  
-  // input offset:
-  const int offset = pixidx*nPlanes + nPlanes*gridDim.x*gridDim.y*blockIdx.z;
+   /* blockIdx.z  = [ 0, bs    ] ()
+      blockIdx.y  = [ 0, ih    ] ()
+      threadIdx.x = [ 0, 31    ] ()
+   */
 
-  // move pointers
-  input  += offset;
-  output += offset;  
-  z 	 += offset;
+   float alphan=alpha/n;
+   
+   __shared__ float pixvalues[maxnumplanes];
+   
+   input  += blockIdx.z*str0 + blockIdx.y*str1;
+   output += blockIdx.z*str0 + blockIdx.y*str1;
+   zsave  += blockIdx.z*str0 + blockIdx.y*str1;
+   
 
-  float alphan = alpha / n;
-  int i,j;
-
-
-  __shared__ float pixvalues[maxnumplanes];
-
-// coalesced read...
-  for (i=0; i<valuesperthread; i++) {
-	pixvalues[i*blk + tidx] = input[i*blk + tidx];
-  }
-
-  for (i=0; i<valuesperthread; i++) {
-         float tmpz = 0;
-         int startf = i*blk + tidx - n/2;
-         int endf = startf + n;
-
-         for(j=startf; j<endf; j++)
-           {
-		if(j>-1 && j<nPlanes)
-                tmpz += pixvalues[j]*pixvalues[j];
-           }
-	tmpz=pow(k+tmpz*alphan,-beta);
-	z[i*blk + tidx]=tmpz;
-	output[i*blk + tidx]=tmpz*pixvalues[i*blk + tidx];
-  }
+   // for each pixel 0 -> iw
+   for (int pixw=0; pixw<iw; pixw++)
+   {
+      // load pixels in shared memory
+      for (int i=threadIdx.x; i<nplanes;  i+=blockDim.x)
+      {
+         pixvalues[i]=input[pixw*nplanes+i];   
+      }
+      
+      for (int i=threadIdx.x; i<nplanes;  i+=blockDim.x)
+      {
+         float z=0;
+         int startf = i - n/2;
+         int endf   = startf + n;
+         startf = (startf < 0) ? 0 : startf;
+         endf = (endf > nplanes) ? nplanes : endf;
+         
+         for (int j=startf; j<endf; j++)
+         {
+            if(j>-1 && j<nplanes)
+            {
+               z += pixvalues[j]*pixvalues[j];
+            }
+         }
+         z=k+z*alphan;
+         zsave[pixw*nplanes+i]=z;
+         output[pixw*nplanes+i]=pixvalues[i]*pow(z,-beta);
+      }
+   }
 }
 
 
-
-
-
-template <int maxnumplanes> __global__ void CrossMapNormalization_gradInput(float *input, float* gradOutput, float* gradInput, float *z, const int nPlanes, const float k, const float alpha, const float beta, const int n)
+template <int maxnumplanes> __global__ void CrossMapNormalization_gradInput(float *input, float* gradOutput, float* gradInput, float *zsave, int iw, int nplanes, const float k, const float alpha, const float beta, const int n, int str0, int str1)
 {
-  // one block = one pixel (x size feature maps)
-  const int pixidx = blockIdx.y * gridDim.x + blockIdx.x;
-  const int blk	= blockDim.x;
-  const int tidx = threadIdx.x;
-  const int valuesperthread = nPlanes/blockDim.x;
-  
-  // input offset:
-  const int offset = pixidx*nPlanes + nPlanes*gridDim.x*gridDim.y*blockIdx.z;
+   /* blockIdx.z  = [ 0, bs    ] ()
+      blockIdx.y  = [ 0, ih    ] ()
+      threadIdx.x = [ 0, 31    ] ()
+   */
 
-  // move pointers
-  input  	+= offset;
-  gradOutput 	+= offset;  
-  gradInput 	+= offset;  
-  z 	 	+= offset;
+   float alphan=alpha/n;
 
-  int i,j;
-
-  float alphan = alpha / n;
-
-  __shared__ float pixvalues[maxnumplanes];
-  __shared__ float gradoutvalues[maxnumplanes];
-  __shared__ float zvalues[maxnumplanes];
-
-// coalesced reads...
-  for (i=0; i<valuesperthread; i++) {
-	pixvalues[i*blk + tidx] = input[i*blk + tidx];
-	gradoutvalues[i*blk + tidx] = gradOutput[i*blk + tidx];
-	zvalues[i*blk + tidx] = z[i*blk + tidx];
-  }
-
-      for(i = 0; i < valuesperthread; i++)
-       {
-         float gradi = 0;
-         float ai = pixvalues[i*blk + tidx];
-         int endo = i*blk + tidx + n/2 + 1;
+   __shared__ float pixvalues[maxnumplanes];
+   __shared__ float gradoutvalues[maxnumplanes];
+   __shared__ float zvalues[maxnumplanes];
+   
+   input  += blockIdx.z*str0 + blockIdx.y*str1;
+   gradOutput += blockIdx.z*str0 + blockIdx.y*str1;
+   gradInput += blockIdx.z*str0 + blockIdx.y*str1;
+   zsave  += blockIdx.z*str0 + blockIdx.y*str1;
+   
+   // for each pixel 0 -> iw
+   for (int pixw=0; pixw<iw; pixw++)
+   {
+      // load pixels in shared memory
+      for (int i=threadIdx.x; i<nplanes;  i+=blockDim.x)
+      {
+         pixvalues[i]=input[pixw*nplanes+i];
+         gradoutvalues[i]=gradOutput[pixw*nplanes+i];   
+         zvalues[i]=zsave[pixw*nplanes+i];   
+      }
+      
+      for (int i=threadIdx.x; i<nplanes;  i+=blockDim.x)
+      {
+         float gradi=0;
+         float ai = pixvalues[i];
+         int endo = i + n/2 + 1;
          int starto = endo - n;
-         for (j=starto; j<endo; j++)
-           {
-		if(j>-1 && j< nPlanes) {
-			float aj = pixvalues[j];
-			float gj = gradoutvalues[j];
-			gradi += (i*blk + tidx == j) ? gj * pow(zvalues[j], -beta) : 0;
-			gradi -= gj * 2 * alphan * beta * ai * aj * pow(zvalues[j], -beta-1);
-		}
-           }
-         gradInput[i*blk + tidx]=gradi;
-       }
 
+         for (int j=starto; j<endo; j++)
+         {
+            if(j>-1 && j<nplanes)
+            {
+      			float aj = pixvalues[j];
+      			float gj = gradoutvalues[j];
+      			gradi += (i == j) ? gj * pow(zvalues[j], -beta) : 0;
+      			gradi -= gj * 2 * alphan * beta * ai * aj * pow(zvalues[j], -beta-1);
+            }
+         }
+         gradInput[pixw*nplanes+i]=gradi;
+      }
+   }
 }
-
-
 
 
 
@@ -125,7 +121,7 @@ static int cunxn_CrossMapNormalization_updateOutput(lua_State *L)
   float alpha = luaT_getfieldchecknumber(L, 1, "alpha");
   float beta = luaT_getfieldchecknumber(L, 1, "beta");
   float k = luaT_getfieldchecknumber(L, 1, "k");
-  long n = luaT_getfieldcheckint(L, 1, "n");
+  int n = luaT_getfieldcheckint(L, 1, "n");
 
 
   input = THCudaTensor_newContiguous(input); // should be contiguous already
@@ -138,39 +134,48 @@ static int cunxn_CrossMapNormalization_updateOutput(lua_State *L)
   float *output_data = THCudaTensor_data(output);
   float *z_data = THCudaTensor_data(z);
 
-  long bs = input->size[0];
-  long isize1 = input->size[1];
-  long isize2 = input->size[2];
-  long nPlanes = input ->size[3];
+  int bs       = input->size[0];
+  int ih       = input->size[1];
+  int iw       = input->size[2];
+  int nPlanes  = input->size[3];
+
+  int str0     = input->stride[0];
+  int str1     = input->stride[1];
 
   assert(nPlanes < 4097); // number of planes must be at most 4096 (or there will be shared memory issues...)
 
   // cuda blocks & threads:
-  dim3 blocks(isize1, isize2, bs);
+  dim3 blocks(1, ih, bs);
   dim3 threads(32);
 
   // kernel:
-
+   /*
+   template <int maxnumplanes> __global__ void CrossMapNormalization_output(float *input, float *output, float *zsave, int iw, int nplanes, const float k, const float alpha, const float beta, const int n, int str0, int str1)
+         blockIdx.z  = [ 0, bs    ] ()
+         blockIdx.y  = [ 0, ih    ] ()
+         threadIdx.x = [ 0, 31    ] ()
+   */   
+   
 	if (nPlanes >3072) {
-  		CrossMapNormalization_output <4096> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+  		CrossMapNormalization_output <4096> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >2048) {
-  		CrossMapNormalization_output <3072> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <3072> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >1536) {
-  		CrossMapNormalization_output <2048> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <2048> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >1024) {
-  		CrossMapNormalization_output <1536> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <1536> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >768) {
-  		CrossMapNormalization_output <1024> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <1024> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >512) {
-  		CrossMapNormalization_output <768> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <768> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >384) {
-  		CrossMapNormalization_output <512> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <512> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >256) {
-  		CrossMapNormalization_output <384> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+	   CrossMapNormalization_output <384> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else if (nPlanes >128) {
-  		CrossMapNormalization_output <256> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+  		CrossMapNormalization_output <256> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	} else {
-  		CrossMapNormalization_output <128> <<<blocks, threads>>> (input_data, output_data, z_data, nPlanes, k, alpha, beta, n);	  
+  		CrossMapNormalization_output <128> <<<blocks, threads>>> (input_data, output_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);	  
 	}
 
 
@@ -219,39 +224,48 @@ static int cunxn_CrossMapNormalization_updateGradInput(lua_State *L)
   float *z_data = THCudaTensor_data(z);
 
 
-  long bs = input->size[0];
-  long isize1 = input->size[1];
-  long isize2 = input->size[2];
-  long nPlanes = input ->size[3];
+  int bs       = input->size[0];
+  int ih       = input->size[1];
+  int iw       = input->size[2];
+  int nPlanes  = input->size[3];
+
+  int str0     = input->stride[0];
+  int str1     = input->stride[1];
 
   assert(nPlanes < 4097); // number of planes must be at most 4096 (or there will be shared memory issues...)
 
   // cuda blocks & threads:
-  dim3 blocks(isize1, isize2,bs);
+  dim3 blocks(1, ih, bs);
   dim3 threads(32);
   
   // kernel:
+/*
+template <int maxnumplanes> __global__ void CrossMapNormalization_gradInput(float *input, float* gradOutput, float* gradInput, float *zsave, int iw, int nPlanes, const float k, const float alpha, const float beta, const int n, int str0, int str1)
+      blockIdx.z  = [ 0, bs    ] ()
+      blockIdx.y  = [ 0, ih    ] ()
+      threadIdx.x = [ 0, 31    ] ()
+   */
 
 	if (nPlanes >3072) {
-		CrossMapNormalization_gradInput <4096> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <4096> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >2048) {
-		CrossMapNormalization_gradInput <3072> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <3072> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >1536) {
-		CrossMapNormalization_gradInput <2048> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <2048> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >1024) {
-		CrossMapNormalization_gradInput <1536> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <1536> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >768) {
-		CrossMapNormalization_gradInput <1024> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <1024> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >512) {
-		CrossMapNormalization_gradInput <768> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <768> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >384) {
-		CrossMapNormalization_gradInput <512> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <512> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >256) {
-		CrossMapNormalization_gradInput <384> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <384> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else if (nPlanes >128) {
-		CrossMapNormalization_gradInput <256> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <256> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	} else {
-		CrossMapNormalization_gradInput <128> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, nPlanes, k, alpha, beta, n);
+		CrossMapNormalization_gradInput <128> <<<blocks, threads>>> (input_data, gradOutput_data, gradInput_data, z_data, iw, nPlanes, k, alpha, beta, n, str0, str1);
 	}
 
 
