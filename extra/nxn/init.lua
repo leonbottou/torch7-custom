dacea42e4babd60b7687d30a0f0585954f1d20f2
Maxime Oquab <moquab@tesla.rocq.inria.fr>
2014-05-26 18:41:48 +0200

    removed convproto

--- extra/nxn/init.lua
+++ extra/nxn/init.lua
@@ -4,7 +4,6 @@ require('libnxn')
 include('Module.lua')
 include('Sequential.lua')
 include('Column.lua')
---include('ConvLayer.lua')
 include('SpatialConvolution.lua')
 include('Copy.lua')
 include('Reshape.lua')
@@ -35,17 +34,6 @@ include('TexFunRandResize.lua')
 include('NeuralNet.lua')
 include('Dataset.lua')
 
-
-
-if false then 
-
-include('LargeDataset.lua')
-
-
-include('Linear.lua')
-
-include('ConvProto.lua')
-end
 
 include('testSgemm.lua')
 
