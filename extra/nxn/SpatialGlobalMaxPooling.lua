b73363909e63e08c2841266817a6d5dc06252f5c
Maxime Oquab <moquab@tesla.rocq.inria.fr>
2014-07-10 15:22:27 +0200

    added description in spatialglobalmaxpooling

--- extra/nxn/SpatialGlobalMaxPooling.lua
+++ extra/nxn/SpatialGlobalMaxPooling.lua
@@ -1,5 +1,18 @@
 local SpatialGlobalMaxPooling, parent = torch.class('nxn.SpatialGlobalMaxPooling', 'nxn.Module')
 
+local help_str = 
+[[This is the global max-pooling module.
+It performs a global max-pooling on each feature map and returns a batchsize*1*1*channels output.
+
+Usage : m = nxn.SpatialGlobalMaxPooling(poolW, poolH, dW, dH)
+
+It only works in BATCH MODE (4D) :
+- with the following input layout : (batch, y, x, channels).
+- channels are the contiguous dimension.
+- a single image must be a (1, y, x, channels) tensor.
+
+The module doesn't require fixed-size inputs.]]
+
 function SpatialGlobalMaxPooling:__init()
    parent.__init(self)
 
