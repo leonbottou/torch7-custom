f1db967154fa6bee90077aaaa13656985ccb9f3d
Maxime Oquab <moquab@tesla.rocq.inria.fr>
2014-08-17 02:43:48 +0200

    put back cmake options modifs + improvements on max pooling

--- extra/cuda/pkg/cunxn/SpatialMaxPooling.cu
+++ extra/cuda/pkg/cunxn/SpatialMaxPooling.cu
@@ -10,12 +10,13 @@
 #define MAX(a,b) (a) > (b) ? (a) : (b)
 
 
-__global__ void maxPool(float *ptrinput, float *ptroutput, const int isize1, const int isize2, const int outsize1, const int outsize2, const int nOutputPlane, const int poolH, const int poolW, const int pooldH, const int pooldW)
+__global__ void maxPool(float *ptrinput, float *ptroutput, const int isize1, const int isize2, const int outsize1, const int outsize2, const int nOutputPlane, const int poolH, const int poolW, const int pooldH, const int pooldW, const int batchsize)
 {
 	// each thread does a pixel of the output
 	const int pixi = blockIdx.x;
 	const int pixj = blockIdx.y;
-	const int bidx = blockIdx.z;
+	const int bidx = blockIdx.z*blockDim.z+threadIdx.z;
+	if(bidx>=batchsize) return;
 
 	int i,j,k;
 
@@ -27,18 +28,6 @@ __global__ void maxPool(float *ptrinput, float *ptroutput, const int isize1, con
 //	const int stridek = (isize1 - poolH) * isize2 * nOutputPlane;
 	float * ptrinputsave = ptrinput;
 
-/*	for(k=0; k<valuesperthread; k++) {
-		float out=-2e38; 
-		for(i=0; i<poolH; i++) {
-			for(j=0; j<poolW; j++) {
-				out=MAX(out, ptrinput[k*blk+tidx]);
-				ptrinput += stridej;
-			}
-			ptrinput += stridei;
-		}
-		ptroutput[k*blk+tidx]=out;
-		ptrinput =ptrinputsave;
-	}	*/
 
 	for(k=threadIdx.x; k<nOutputPlane; k+=blockDim.x) {
 		float out=-2e38; 
@@ -56,7 +45,7 @@ __global__ void maxPool(float *ptrinput, float *ptroutput, const int isize1, con
 }
 
 
-__global__ void maxPoolBackward(float *ptrinput, float *ptroutput, float *ptrgradinput, float *ptrgradoutput, const int isize1, const int isize2, const int outsize1, const int outsize2, const int nOutputPlane, const int poolH, const int poolW, const int pooldH, const int pooldW)
+__global__ void maxPoolBackward(float *ptrinput, float *ptroutput, float *ptrgradinput, float *ptrgradoutput, const int isize1, const int isize2, const int outsize1, const int outsize2, const int nOutputPlane, const int poolH, const int poolW, const int pooldH, const int pooldW, const int batchsize)
 {
 
 	// this one is a bit tricky : we have to add up the gradient if the pooling overlaps...
@@ -66,12 +55,43 @@ __global__ void maxPoolBackward(float *ptrinput, float *ptroutput, float *ptrgra
 
 	const int pixi = blockIdx.x;
 	const int pixj = blockIdx.y;
-	const int bidx = blockIdx.z;
+	const int bidx = blockIdx.z*blockDim.z+threadIdx.z;
+
+	__shared__ int _imin, _jmin, _imax, _jmax;
+	int imin, jmin, imax, jmax;
+	if(threadIdx.z==0)
+	{
+      imin=(pixi - (poolH - 1) + (pooldH -1))/pooldH > 0 ? (pixi - (poolH - 1) + (pooldH -1))/pooldH : 0 ;
+      jmin=(pixj - (poolW - 1) + (pooldW -1))/pooldW > 0 ? (pixj - (poolW - 1) + (pooldW -1))/pooldW : 0 ;
+      imax= pixi / pooldH < outsize1 ? pixi / pooldH : outsize1 - 1 ;
+      jmax= pixj / pooldW < outsize2 ? pixj / pooldW : outsize2 - 1 ;
+		if(threadIdx.x==0)
+		{
+			_imin=imin;
+			_jmin=jmin;
+			_imax=imax;
+			_jmax=jmax;
+		}
+	}
 	
-        const int imin=(pixi - (poolH - 1) + (pooldH -1))/pooldH > 0 ? (pixi - (poolH - 1) + (pooldH -1))/pooldH : 0 ;
-        const int jmin=(pixj - (poolW - 1) + (pooldW -1))/pooldW > 0 ? (pixj - (poolW - 1) + (pooldW -1))/pooldW : 0 ;
-        const int imax= pixi / pooldH < outsize1 ? pixi / pooldH : outsize1 - 1 ;
-        const int jmax= pixj / pooldW < outsize2 ? pixj / pooldW : outsize2 - 1 ;
+	__syncthreads();
+
+	if(bidx>=batchsize) return;
+
+	if(threadIdx.z>0)
+	{
+		if(threadIdx.x==0)
+		{
+			imin=_imin;
+			jmin=_jmin;
+			imax=_imax;
+			jmax=_jmax;
+		}
+		imin=__shfl(imin,0);
+		jmin=__shfl(jmin,0);
+		imax=__shfl(imax,0);
+		jmax=__shfl(jmax,0);
+	}
 
 	int i,j,k;
 
@@ -107,29 +127,8 @@ __global__ void maxPoolBackward(float *ptrinput, float *ptroutput, float *ptrgra
 		ptroutput = ptroutputsave;
 		ptrgradoutput = ptrgradoutputsave;
 	}	
-	
-/*	for(k=0; k<valuesperthread; k++) {
-		float pixvalue=ptrinput[k*blk+tidx];
-		float gradinputvalue=0;
-		for(i=imin; i<imax+1; i++) {
-			for(j=jmin; j<jmax+1; j++) {
-				float out=ptroutput[k*blk+tidx];
-				if(pixvalue==out) {
-//					ptrgradinput[k*blk+tidx] += ptrgradoutput[k*blk+tidx];
-					gradinputvalue += ptrgradoutput[k*blk+tidx];
-				}
-				ptroutput += stridej;
-				ptrgradoutput += stridej;
-			}
-			ptroutput += stridei;
-			ptrgradoutput += stridei;
-		}
-		ptrgradinput[k*blk+tidx]=gradinputvalue;
-		ptroutput = ptroutputsave;
-		ptrgradoutput = ptrgradoutputsave;
-	}	
-	
-*/
+
+
 }
 
 
@@ -173,10 +172,10 @@ static int cunxn_SpatialMaxPooling_updateOutput(lua_State *L)
 
 
   // cuda blocks & threads:
-  dim3 blocks (outsize1, outsize2, bs);
-  dim3 threads (32);
+  dim3 blocks (outsize1, outsize2, (bs+3)/4);
+  dim3 threads (32,1,4);
 
-  maxPool<<<blocks,threads>>>(ptrinput, ptroutput, isize1, isize2, outsize1, outsize2, isize3, poolH, poolW, dH, dW);
+  maxPool<<<blocks,threads>>>(ptrinput, ptroutput, isize1, isize2, outsize1, outsize2, isize3, poolH, poolW, dH, dW, bs);
 
 
 
@@ -222,8 +221,8 @@ static int cunxn_SpatialMaxPooling_updateGradInput(lua_State *L)
 
   THCudaTensor_resizeAs(gradInput, input);
 
-  dim3 blocks (isize1, isize2, bs);
-  dim3 threads (32);
+  dim3 blocks (isize1, isize2, (bs+7)/8);
+  dim3 threads (32,1,8);
 
   float* ptroutput  = THCudaTensor_data(output);
   float* ptrinput   = THCudaTensor_data(input);
@@ -231,7 +230,7 @@ static int cunxn_SpatialMaxPooling_updateGradInput(lua_State *L)
   float* ptrgradinput   = THCudaTensor_data(gradInput);
 
 
-  maxPoolBackward <<<blocks,threads>>>(ptrinput, ptroutput, ptrgradinput, ptrgradoutput, isize1, isize2, outsize1, outsize2, isize3,  poolH, poolW, dH, dW);
+  maxPoolBackward <<<blocks,threads>>>(ptrinput, ptroutput, ptrgradinput, ptrgradoutput, isize1, isize2, outsize1, outsize2, isize3,  poolH, poolW, dH, dW, bs);
   // check for errors
   cudaError_t err = cudaGetLastError();
   if (err != cudaSuccess) {
